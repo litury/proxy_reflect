9b88b301e91e23011ea7fc8f8a38580529d3e30a
litury <litury@icloud.com>
2022-11-25 10:43:02 +0500

    feat: add JQ

--- LibraryExamples/jQueryExamples.js
+++ LibraryExamples/jQueryExamples.js
@@ -14,30 +14,43 @@
  * --------------------------------------- *
  */
 $ // стандартный объект jQuery
-$( document ).ready(function() { // получение глобального объекта document и выполнение функции при его загрузке
-  console.log( "ready!" );
-});
-$( "#myDivId" ); // получение элемента по id (#)
-$( ".myDivClass" ); // получение элемента по классу
-var myValue = $( "#myDivId" ).val(); // получение значения инпута
-$( "#myDivId" ).val( "hello world" ); // запись значение
-$( "p" ).click(function() { // установка обработчика на click
-  console.log( "You clicked a paragraph!" );
-});
-$( "p" ).on( "click", function() { // та же логика обработчика
-  console.log( "click" );
-});
-$( "p.color" ).click(function () { // установка обработчика на click
-  $(this).toggleClass('color')
-  console.log( "You paint a paragraph!" );
-});
-$( "#menu li" ).hover(function() { 
-  $( this ).toggleClass( "hover" );
-});
-$( "p.hide" ).hide();// спрятать элемент
-// показать все спрятанные div-ы
-$( "div.hidden" ).show();
 
+$(document).ready(function () { // получение глобального объекта document и выполнение функции при его загрузке
+	console.log("ready!");
+});
+
+$("#myDivId"); // получение элемента по id (#)
+
+$(".myDivClass"); // получение элемента по классу
+
+var myValue = $("#myDivId").val(); // получение значения инпута
+
+$("#myDivId").val("hello world"); // запись значение
+
+$("p").click(function () { // установка обработчика на click
+	console.log("You clicked a paragraph!");
+});
+
+$("p").on("click", function () { // та же логика обработчика
+	console.log("click");
+});
+
+$("p.color").click(function () { // установка обработчика на click
+	$(this).toggleClass('color')
+	console.log("You paint a paragraph!");
+});
+
+$("#menu li").hover(function () {
+	$(this).toggleClass("hover");
+});
+
+$("p.hide").click(function () {
+	$("p.hide").hide();// спрятать элемент
+})
+
+$("div.hidden").click(function () {
+	$("p.hide").show();// спрятать элемент
+})
 // Чтобы изменить класс у html элемента
 // стандартный способ 
 const someDiv = document.querySelector('.someClass')
@@ -45,4 +58,4 @@ someDiv?.classList.add('newClass')
 // document.querySelector('.someClass').classList.add('newClass')
 
 //jQuery
-$('.someClass').addClass( "newClass yourClass" );
+$('.someClass').addClass("newClass yourClass");
